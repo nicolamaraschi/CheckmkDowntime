ef4408ecc97c1a1a9d4557eaa5319b74b2c166a9
Nicola Maraschi <nicola.maraschi01@gmail.com>
2025-11-06 11:25:04 +0100

    modifiche

--- dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Avvia il backend in un nuovo terminale
-osascript -e 'tell app "Terminal" to do script "cd '$(pwd)'/backend && pip install -r requirements.txt && uvicorn app.main:app --reload"'
-
-# Avvia il frontend
-npm start
